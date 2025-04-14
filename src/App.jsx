94d17aa1d24211de0ab8ced053e7ad9a5cd2747f
microbjj <1705888@mail.ru>
2025-04-14 16:14:31 +0700

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,93 @@
-import { useState } from 'react'
-import classes from './app.module.css'
+import { useState } from 'react';
+import classes from './app.module.css';
 
 function App() {
-  const initState = {
-    value: '',
-    list: [],
-    error: '',
-  }
-
-  const [state, setState] = useState(initState)
-
-  function onInputButtonClick() {
-    const promptValue = prompt('Введите значение:')
-    const hasError = promptValue?.trim().length >= 3
-
-    if (!hasError) {
-      setState({
-        ...state,
-        error: 'Введеное сообщение должно содержать минимум 3 символа'
-      })
-    } else {
-      setState({
-        ...state, 
-        value: promptValue,
-        error: '',
-      })
-    }
-  }
-
-  function onAddButtonClick(newState) {
-    if (newState.value) {
-      const timestamp = new Date().toLocaleString('ru-RU');
-      setState({
-        ...state,
-        list: [...state.list, {
-          id: Date.now(),
-          value: newState.value,
-          timestamp
-        }],
+    const initState = {
         value: '',
-        error: ''
-      })
+        list: [],
+        error: '',
+    };
+
+    const [state, setState] = useState(initState);
+
+    function onInputButtonClick() {
+        const promptValue = prompt('Введите значение:');
+        const hasError = promptValue?.trim().length >= 3;
+
+        if (!hasError) {
+            setState({
+                ...state,
+                error: 'Введеное сообщение должно содержать минимум 3 символа',
+            });
+        } else {
+            setState({
+                ...state,
+                value: promptValue,
+                error: '',
+            });
+        }
     }
-  }
 
-  const errorMessage = <div className={classes.error}>{state.error}</div>
-  const isValueValid = state.value.length >= 3
+    function onAddButtonClick(newState) {
+        if (newState.value) {
+            const timestamp = new Date().toLocaleString('ru-RU');
+            setState({
+                ...state,
+                list: [
+                    ...state.list,
+                    {
+                        id: Date.now(),
+                        value: newState.value,
+                        timestamp,
+                    },
+                ],
+                value: '',
+                error: '',
+            });
+        }
+    }
 
-  return (
-    <div className={classes.app}>
-      <h1 className={classes.pageHeading}>Ввод значения</h1>
-      <p className={classes.noMarginText}>
-        Текущее значение <code>value</code>: "<output className={classes.currentValue}>{state.value}</output>"
-      </p>
-      {!isValueValid && errorMessage}
-      <div className={classes.buttonsContainer}>
-        <button onClick={onInputButtonClick} className={classes.button}>Ввести новое</button>
-        <button onClick={() => onAddButtonClick(state)} className={classes.button} disabled={!isValueValid}>Добавить в список</button>
-      </div>
-      <div className={classes.listContainer}>
-        <h2 className={classes.listHeading}>Список:</h2>
-          {state.list.length > 0
-            ? <ul className={classes.list}>
-                { state.list.map(item => 
-                  <li key={item.id} className={classes.listItem}>{item.timestamp} - {item.value}</li>
+    const errorMessage = <div className={classes.error}>{state.error}</div>;
+    const isValueValid = state.value.length >= 3;
+
+    return (
+        <div className={classes.app}>
+            <h1 className={classes.pageHeading}>Ввод значения</h1>
+            <p className={classes.noMarginText}>
+                Текущее значение <code>value</code>: "
+                <output className={classes.currentValue}>{state.value}</output>"
+            </p>
+            {!isValueValid && errorMessage}
+            <div className={classes.buttonsContainer}>
+                <button onClick={onInputButtonClick} className={classes.button}>
+                    Ввести новое
+                </button>
+                <button
+                    onClick={() => onAddButtonClick(state)}
+                    className={classes.button}
+                    disabled={!isValueValid}
+                >
+                    Добавить в список
+                </button>
+            </div>
+            <div className={classes.listContainer}>
+                <h2 className={classes.listHeading}>Список:</h2>
+                {state.list.length > 0 ? (
+                    <ul className={classes.list}>
+                        {state.list.map((item) => (
+                            <li key={item.id} className={classes.listItem}>
+                                {item.timestamp} - {item.value}
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p className={classes.noMarginText}>
+                        Нет добавленных элементов
+                    </p>
                 )}
-              </ul>
-            : <p className={classes.noMarginText}>Нет добавленных элементов</p>
-          }
-      </div>
-    </div>
-  )
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001, // Укажите нужный порт, например, 3000
-    host: '127.0.0.1', // Можно дополнительно указать хост, если нужно
-  },
-})
+    plugins: [react()],
+    server: {
+        port: 3001, // Укажите нужный порт, например, 3000
+        host: '127.0.0.1', // Можно дополнительно указать хост, если нужно
+    },
+});
